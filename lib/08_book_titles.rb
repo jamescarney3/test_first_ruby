f45b9aad6cc324bff7881206140d2df8ab88e2bb
James Carney <jmcarneyiii@gmail.com>
2015-04-17 22:55:13 +0000

    completed ex 8

--- lib/08_book_titles.rb
+++ lib/08_book_titles.rb
@@ -1,3 +1,20 @@
 class Book
-  # TODO: your code goes here!
+  def initialize(title = nil)
+    @title = title
+    @stop_words = %w{the a an and in of}
+  end
+  def title=(title)
+    @title = title
+  end
+  def title
+    title_words = []
+    @title.split(" ").each do |word|
+      if not @stop_words.include?(word)
+        word.capitalize!
+      end
+      title_words.push(word)
+    end
+    title_words[0].capitalize!
+    title_words.join(" ")
+  end 
 end
