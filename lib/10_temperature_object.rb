4e8ea6876851cfad2bc377613297bdc0a4d9b300
James Carney <jmcarneyiii@gmail.com>
2015-04-18 02:58:47 +0000

    in progress ex 10, subclasses not working yet

--- lib/10_temperature_object.rb
+++ lib/10_temperature_object.rb
@@ -1,3 +1,45 @@
 class Temperature
   # TODO: your code goes here!
+  def initialize(opts = {})
+    @opts = opts
+    puts opts.to_s + "!"
+  end
+  
+  
+  
+  def in_fahrenheit
+    if @opts.has_key?(:f)
+      @opts[:f]
+    elsif @opts.has_key?(:c)
+      (@opts[:c] * 9.0/5.0 + 32).round(1) 
+    end
+  end
+  
+  def in_celsius
+    if @opts.has_key?(:c)
+      @opts[:c]
+    elsif @opts.has_key?(:f)
+      ((@opts[:f] - 32) * 5.0/9.0).round(1) 
+    end
+  end
+  
+  def self.from_celsius(x)
+    Temperature.new({:c => x})
+  end
+  
+  def self.from_fahrenheit(x)
+    Temperature.new({:f => x})
+  end
 end
+
+class Celsius < Temperature
+
+end
+
+class Fahrenheit < Temperature
+
+end
+
+var = Celsius.new(50)
+
+
