fe7d1a70d10f75995e7fe7ff65791da08295dc8f
James Carney <jmcarneyiii@gmail.com>
2015-04-20 00:23:33 +0000

    ex 12 basic arithmetic operations functional

--- lib/12_rpn_calculator.rb
+++ lib/12_rpn_calculator.rb
@@ -1,3 +1,44 @@
 class RPNCalculator
-  # TODO: your code goes here!
+  
+  attr_accessor :value
+  
+  def initialize
+    @stack = []
+    @value = 0
+  end
+  
+  def push(num)
+    @stack.push(num)
+  end
+  
+  def plus
+    num1 = @stack.pop
+    num2 = @stack.pop
+    @value = num2 + num1
+    @stack.push(@value)
+  end
+  
+  def minus
+    num1 = @stack.pop
+    num2 = @stack.pop
+    @value = num2 - num1
+    @stack.push(@value)
+  end
+  
+  def times
+    num1 = @stack.pop
+    num2 = @stack.pop
+    @value = num2 * num1
+    @stack.push(@value)
+  end
+  
+  def divide
+    num1 = @stack.pop
+    num2 = @stack.pop
+    @value = num2.to_f / num1.to_f
+    @stack.push(@value)
+  end
+    
+    
+    
 end
