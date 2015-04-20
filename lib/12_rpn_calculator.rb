f6e707ddbf959d05c7a7f4178444bcc8cec30b9b
James Carney <jmcarneyiii@gmail.com>
2015-04-20 00:47:30 +0000

    completed ex 12

--- lib/12_rpn_calculator.rb
+++ lib/12_rpn_calculator.rb
@@ -1,44 +1,84 @@
-class RPNCalculator
+class RPNCalculator < Array
   
   attr_accessor :value
   
   def initialize
-    @stack = []
     @value = 0
   end
   
-  def push(num)
-    @stack.push(num)
-  end
-  
   def plus
-    num1 = @stack.pop
-    num2 = @stack.pop
+    if self.length < 2
+      raise "calculator is empty"
+    end
+    num1 = self.pop
+    num2 = self.pop
     @value = num2 + num1
-    @stack.push(@value)
+    self.push(@value)
   end
   
   def minus
-    num1 = @stack.pop
-    num2 = @stack.pop
+    if self.length < 2
+      raise "calculator is empty"
+    end
+    num1 = self.pop
+    num2 = self.pop
     @value = num2 - num1
-    @stack.push(@value)
+    self.push(@value)
   end
   
   def times
-    num1 = @stack.pop
-    num2 = @stack.pop
+    if self.length < 2
+      raise "calculator is empty"
+    end
+    num1 = self.pop
+    num2 = self.pop
     @value = num2 * num1
-    @stack.push(@value)
+    self.push(@value)
   end
   
   def divide
-    num1 = @stack.pop
-    num2 = @stack.pop
+    if self.length < 2
+      raise "calculator is empty"
+    end
+    num1 = self.pop
+    num2 = self.pop
     @value = num2.to_f / num1.to_f
-    @stack.push(@value)
+    self.push(@value)
   end
-    
-    
-    
+  
+  def tokens(input)
+    strokes = []
+    input.split(" ").each do |stroke|
+      if stroke.match(/\d/)
+        strokes.push(stroke.to_i)
+      elsif stroke == "+"
+        strokes.push(:+)
+      elsif stroke == "-"
+        strokes.push(:-)
+      elsif stroke == "*"
+        strokes.push(:*)
+      elsif stroke == "/"
+        strokes.push(:/)
+      end
+    end
+    strokes
+  end
+  
+  def evaluate(input)
+    tokens(input).each do |stroke|
+      if stroke.class == Fixnum
+        self.push(stroke.to_i)
+      elsif stroke == :+
+        self.plus
+      elsif stroke == :-
+        self.minus
+      elsif stroke == :*
+        self.times
+      elsif stroke == :/
+        self.divide
+      end
+    end
+    @value
+  end
+  
 end
